30af996154093c1e83d468e268c79499e7bb2e82
msyao-github <msyaois@gmail.com>
2016-03-13 22:04:15 -0400

    included edit survey content in index.js/html files

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,14 +6,16 @@ let access = require('./api/access.js');
 // handlebars template require below
 let navbarTemplate = require('./handlebars/navbar.handlebars');
 // let modalTemplate = require('./handlebars/sign-modal.handlebars');
-let surveyTemplate = require('./handlebars/create-survey.handlebars');
+let createTemplate = require('./handlebars/create-survey.handlebars');
+let editTemplate = require('./handlebars/edit-survey.handlebars');
 let dashTemplate = require('./handlebars/dashboard.handlebars');
 
 let init = function() {
   // Main Functionaliy
   // $('.modal-body').append(modalTemplate());
   $('.navbar').append(navbarTemplate());
-  $('.create-survey-page').append(surveyTemplate());
+  $('.create-survey-page').append(createTemplate());
+  $('.edit-survey-page').append(editTemplate());
   $('.dashboard-page').append(dashTemplate());
   $('#register').on('submit', access.signUp);
   $('#logIn').on('submit', access.signIn);
