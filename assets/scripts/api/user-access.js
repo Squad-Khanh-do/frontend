188418ea064d1410a551d54cd606668828efcedb
msyao-github <msyaois@gmail.com>
2016-03-14 10:46:47 -0400

    edited user access js

--- assets/scripts/api/user-access.js
+++ assets/scripts/api/user-access.js
@@ -1,7 +1,5 @@
 'use strict';
 
-//const below lets you edit the home url in one place instead of inside each form. You can use as part of a path to change values
-//This script works with index.html
 const myApp = {
   baseUrl: 'http://localhost:3000',
 };
