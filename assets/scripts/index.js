b12569436fe80a9afce39a6c82f6393e455c3901
Ethan <erdesi90562@gmail.com>
2016-03-16 10:36:40 -0400

    fix change password issue

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -10,7 +10,7 @@ let init = function() {
   $('#sign-up').on('submit', user.signUp);
   $('#sign-in').on('submit', user.signIn);
   $('#change-password').on('submit', user.changePassword);
-  $('#sign-out-button').on('submit', user.signOut);
+  $('#sign-out-button').on('click', user.signOut);
   // Other functions below
   $('.change-password-nav').hide();
   $('.sign-out-nav').hide();
