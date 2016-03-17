5fbeed0a75961b5983c7e4d8668da43f75cec674
Ethan <erdesi90562@gmail.com>
2016-03-16 23:23:32 -0400

    forgot the fucking comma

--- assets/scripts/myApp.js
+++ assets/scripts/myApp.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const myApp = {
-  baseUrl: 'https://take-a-vey.herokuapp.com'
+  baseUrl: 'https://take-a-vey.herokuapp.com',
   baseUrl2: 'http://localhost:3000',
   baseUrl3: 'http://httpbin.org/post,'
 };
