39253ccb0a27669ff7d61ae3b1ab0e2b9634ede0
Ethan <erdesi90562@gmail.com>
2016-03-16 20:02:25 -0400

    add update functionality to dashboard

--- assets/scripts/ui/survey.js
+++ assets/scripts/ui/survey.js
@@ -6,13 +6,28 @@ let log = function (data) {
   console.log(data);
 };
 
+let renderDash = function (survey) {
+  let dashTemplate = require('../handlebars/dashboard.handlebars');
+  $('.dashboard-page').html(dashTemplate({survey}));
+    uiDeleteSurvey();
+    uiUpdateSurvey();
+};
+
+let refreshDash = function () {
+  $('.create-survey-page').empty();
+  api.retrieveSurveys(renderDash, log);
+};
+
 $('.survey-tab').on('click', function () {
   let createTemplate = require('../handlebars/create-survey.handlebars');
   $('.create-survey-page').html(createTemplate());
   $('.dashboard-page').empty();
   $('.create-survey-submit').on('click', function(){
     console.log("submit works");
-    api.createSurvey('#createSurvey', refreshDash, log);
+    api.createSurvey('#createSurvey', function () {
+      $('.create-survey-page').empty();
+      api.retrieveSurveys(renderDash, log);
+    }, log);
   });
 });
 
@@ -31,18 +46,23 @@ let uiDeleteSurvey = function() {
   });
 };
 
-let renderDash = function (survey) {
-  let dashTemplate = require('../handlebars/dashboard.handlebars');
-  $('.dashboard-page').html(dashTemplate({survey}));
-    uiDeleteSurvey();
-};
-
-let refreshDash = function () {
-  $('.create-survey-page').empty();
-  api.retrieveSurveys(renderDash, log);
-  // add button controller JQ
+let uiUpdateSurvey = function() {
+  $('.update-button').on('click', function(e) {
+    e.preventDefault();
+    var surveyId = $(this).attr('data-results-id');
+    api.getOneSurvey(surveyId, function (survey) {
+      let createSurveyTemplate = require('../handlebars/create-survey.handlebars');
+      $('.dashboard-page').empty();
+      $('.create-survey-page').html(createSurveyTemplate({survey}));
+      $('.create-survey-submit').on('click', function(){
+        api.updateSurvey(surveyId, '#createSurvey', log, refreshDash); // HACK: ummm idk why refreshDash needs to be in failure
+      });
+    }, log);
+  });
 };
 
 $('.dashboard-tab').on('click', refreshDash);
 
-module.exports = true;
+module.exports = {
+  refreshDash,
+};
