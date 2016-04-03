d6d44487c5798aa41e0362a1d721bf1307913d7d
msyao-github <msyaois@gmail.com>
2016-04-03 08:39:22 -0400

    commit bundle

--- vendor.bundle.js
+++ vendor.bundle.js
@@ -100,7 +100,7 @@
 /***/ function(module, exports, __webpack_require__) {
 
 	__webpack_require__(2);
-	module.exports = __webpack_require__(45);
+	module.exports = __webpack_require__(44);
 
 
 /***/ },
@@ -9943,7 +9943,7 @@
 
 /***/ },
 
-/***/ 45:
+/***/ 44:
 /***/ function(module, exports, __webpack_require__) {
 
 	/* WEBPACK VAR INJECTION */(function(jQuery) {/*!
